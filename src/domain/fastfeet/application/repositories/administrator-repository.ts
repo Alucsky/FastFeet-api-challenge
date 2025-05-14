e1d3e893ea246f958ae8c96bbbbc38e8b8c3df18
Samuel Molinari <samuelmolinari8@gmail.com>
2025-05-14 16:42:40 -0300

    to not interfer in nexts steps, implementing the inMemmory

--- src/domain/fastfeet/application/repositories/administrator-repository.ts
+++ src/domain/fastfeet/application/repositories/administrator-repository.ts
@@ -1,8 +1,8 @@
 import { Administrator } from "../../enterprise/entities/Administrator";
 
 export interface AdministratorRepository {
-  findById(id: string): Promise<Administrator>;
+  // findById(id: string): Promise<Administrator>;
   create(administrator: Administrator): Promise<Administrator>;
-  update(administrator: Administrator): Promise<Administrator>;
-  delete(administratorId: string): Promise<void>;
+  // update(administrator: Administrator): Promise<Administrator>;
+  // delete(administratorId: string): Promise<void>;
 }
